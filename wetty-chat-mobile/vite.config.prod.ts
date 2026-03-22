1489c0140be088e14f706d677e4dd8118f1ada63
nanamicat <nanamicat77@gmail.com>
2026-03-22 23:58:05 +0800

    api url

--- wetty-chat-mobile/vite.config.prod.ts
+++ wetty-chat-mobile/vite.config.prod.ts
@@ -2,11 +2,8 @@ import { defineConfig, mergeConfig } from 'vite';
 import { createBaseConfig } from './vite.config.base';
 import { execSync } from 'child_process';
 
-const assetUrl = process.env.ASSET_URL?.replace(/\/+$/, '');
-
-if (!assetUrl) {
-  throw new Error('ASSET_URL is required for production builds');
-}
+const ASSET_URL = 'https://chahui.app';
+const API_BASE_URL = `${ASSET_URL}/_api`;
 
 let commitHash = 'unknown';
 try {
@@ -15,19 +12,20 @@ try {
   // Ignore
 }
 
-export default mergeConfig(createBaseConfig({ assetCdnOrigin: assetUrl }), defineConfig({
+export default mergeConfig(createBaseConfig({ assetCdnOrigin: ASSET_URL }), defineConfig({
   experimental: {
     renderBuiltUrl(filename, { type }) {
       if (type === 'public') {
         return `/${filename}`;
       }
 
-      return `${assetUrl}/${filename}`;
+      return `${ASSET_URL}/${filename}`;
     },
   },
   define: {
     // Uncomment this and comment out __AUTH_REDIRECT_URL__ for separate domain deployment
-    // __API_BASE__: JSON.stringify('https://chahui.app/_api'),
+    __ASSET_BASE__: JSON.stringify(ASSET_URL),
+    __API_BASE__: JSON.stringify(API_BASE_URL),
     __APP_VERSION__: JSON.stringify(commitHash),
     __AUTH_REDIRECT_URL__: JSON.stringify("/main/member.php?mod=logging&action=login&referer=https://www.shireyishunjian.com/chat/"),
   },
